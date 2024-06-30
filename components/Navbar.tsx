2a194f95c6f3ac0e7fd675778704a79f9b5ea179
NagaprasadVr <nagaprasadvr246@gmail.com>
2024-06-30 10:34:35 +0530

    styles2

--- components/Navbar.tsx
+++ components/Navbar.tsx
@@ -5,7 +5,7 @@ export const Navbar = () => {
     <nav className="w-full h-20 justify-start flex flex-row items-center pl-5">
       <div className="flex flex-row justify-center items-center border-2 border-white p-2 rounded-lg gap-1 h-12">
         <Image src="/atom2.svg" width={30} height={30} alt="logo" />
-        <p className="text-md p-1 bg-[#ccf7ff] rounded text-black font-semibold">
+        <p className="text-md p-1 bg-[#ccf7ff] rounded text-black font-bold">
           OxPublish
         </p>
       </div>
